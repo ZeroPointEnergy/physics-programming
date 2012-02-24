e2f0f456f872330eaed17318280defce4f2a75fa
Andreas Zuber <zuber@puzzle.ch>
2012-02-24 11:46:08 +0100

    first prog :-)

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+/* This is my first C program
+ * just to say hello to the world
+ */
+
+#include <stdio.h>
+
+int main(void) 
+{
+  printf("Hello World\n");
+  printf("Hello Tobias\n");
+  return 0;
+}
